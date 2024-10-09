e7acc273866eb6bf6ae2abfade6083c000939582
21MoududAhammed <21moudud@gmail.com>
2024-10-09 11:04:29 +0600

    Now the banner is responsive

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,17 +1,45 @@
 import heroImg from "../../assets/hero.webp";
+import { MdOutlineMyLocation } from "react-icons/md";
+
 export default function Banner() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 ps-5">
+    <div className="grid grid-cols-1 md:grid-cols-2  max-w-screen-2xl mx-auto px-5">
       <div className="flex flex-col justify-center  justify- font-gilory space-y-5">
-        <h1 className="text-7xl font-bold text-primary">Fast, Fresh</h1>
-        <h1 className="text-7xl font-bold">
+        {/* headings  */}
+        <h1 className=" text-4xl sm:text-6xl  md:text-7xl font-bold text-primary">Fast, Fresh</h1>
+        <h1 className="text-3xl sm:text-5xl md:text-7xl font-bold">
           <span className="text-primary">& Right</span>{" "}
           <span className="text-secondary">To Your Door</span>
         </h1>
-        <h3 className='text-2xl text-secondary'>Order dishes from favorite restaurants near you.</h3>
+        <h3 className=" text-xl sm:text-2xl text-secondary ">
+          Order dishes from favorite restaurants near you.
+        </h3>
+        {/* search / find food  */}
+        <div className="flex items-center justify-center border-2 border-primary rounded-md relative">
+          <div className="w-full p-2">
+            {" "}
+            <input
+              className="w-full py-3 px-2 focus:outline-none"
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Enter the location"
+            />{" "}
+          </div>
+          <div className="bg-primary w-40 h-full flex justify-center items-center">
+            {" "}
+            <button className="text-xl sm:text-2xl font-bold text-white">Find Food</button>
+          </div>
+          <div className="absolute right-28 sm:right-36 text-primary font-semibold ">
+            <button className="flex justify-center items-center gap-1">
+              <MdOutlineMyLocation /> 
+              {/* Locate me */}
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="flex justify-center items-center ">
-        <img className="w-1/2" src={heroImg} alt="hero section image" />
+      <div className="flex justify-center items-center">
+        <img className="w-3/4" src={heroImg} alt="hero section image" />
       </div>
     </div>
   );
--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -4,40 +4,42 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 export default function Nav() {
   return (
-    <nav className="flex items-center justify-between px-8 py-3 shadow">
-      <div className="flex justify-center items-center gap-2">
-        <img className="w-12 rounded-full" src={logo} alt="logo" />
-        <h2 className="text-2xl font-bold font-serif hidden sm:block">
-          Foodie Haven
-        </h2>
-      </div>
-      <div className="flex justify-center items-center gap-5">
-        <ul className="flex justify-center items-center gap-3">
-          <li>
-            <p className="text-2xl">
-              <MdFavoriteBorder />
-            </p>
-          </li>
-          <li>
-            <p className="text-2xl">
-              <IoMdNotificationsOutline />
-            </p>
-          </li>
-          <li>
-            <p className="text-2xl">
-              <AiOutlineShoppingCart />
-            </p>
-          </li>
-        </ul>
-        <div className="flex items-center justify-center gap-3">
-          <div className="bg-red-400 w-10 h-10  rounded-full flex justify-center items-center">
-            <p className='text-white font-bold text-xl'>M</p>
-          </div>
-          <div>
-            <button>Sign In</button>
+    <section className="shadow font-gilory mb-10 md:mb-0">
+      <nav className="flex items-center justify-between mx-auto py-3 md:py-5 px-3 sm:px-5 max-w-screen-2xl">
+        <div className="flex justify-center items-center gap-2">
+          <img className="w-12 rounded-full" src={logo} alt="logo" />
+          <h2 className="text-2xl font-bold font-serif hidden sm:block">
+            Foodie Haven
+          </h2>
+        </div>
+        <div className="flex justify-center items-center gap-5">
+          <ul className="flex justify-center items-center gap-3">
+            <li>
+              <p className="text-2xl">
+                <MdFavoriteBorder />
+              </p>
+            </li>
+            <li>
+              <p className="text-2xl">
+                <IoMdNotificationsOutline />
+              </p>
+            </li>
+            <li>
+              <p className="text-2xl">
+                <AiOutlineShoppingCart />
+              </p>
+            </li>
+          </ul>
+          <div className="flex items-center justify-center gap-3">
+            <div className="bg-red-400 w-10 h-10  rounded-full flex justify-center items-center">
+              <p className="text-white font-bold text-xl">M</p>
+            </div>
+            <div>
+              <button>Sign In</button>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </section>
   );
 }
